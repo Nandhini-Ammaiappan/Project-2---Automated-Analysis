732e2296a182d10d1f3178a3769432693c12d72b
Nandhini-Ammaiappan <24f1002469@ds.study.iitm.ac.in>
2024-12-10 23:09:32 +0530

    Create autolysis.py

---
+++ autolysis.py
@@ -0,0 +1,18 @@
+import requests
+import os
+
+#api_key = "eyJhbGciOiJIUzI1NiJ9.eyJlbWFpbCI6IjI0ZjEwMDI0NjlAZHMuc3R1ZHkuaWl0bS5hYy5pbiJ9.fi0afXSQetfhBPAgCm1rEeiIoYBlFzQi-t1W4Q12CV0"
+api_key = os.environ["AIPROXY_TOKEN"] 
+print(api_key)
+response = requests.post("https://aiproxy.sanand.workers.dev/openai/v1/chat/completions",
+    headers={"Authorization": f"Bearer {api_key}"},
+    json={
+        "model": "gpt-4o-mini",
+        "messages": [
+            {"role": "system", "content": "You are a helpful assistant."},
+            {"role": "user", "content": "How do I use a proxy with ChatGPT?"},
+            ]
+        }
+    )
+result = response.json()
+print(result)
