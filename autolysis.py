ddfb9230eb45d6e69b77eb946213ad5d59161645
Nandhini-Ammaiappan <24f1002469@ds.study.iitm.ac.in>
2024-12-11 07:44:14 +0530

    Imp

--- autolysis.py
+++ autolysis.py
@@ -6,11 +6,13 @@
 # ]
 # ///
 
+!pip install python-dotenv
 import requests
 import os
+from dotenv import load_dotenv
 
 #api_key = "eyJhbGciOiJIUzI1NiJ9.eyJlbWFpbCI6IjI0ZjEwMDI0NjlAZHMuc3R1ZHkuaWl0bS5hYy5pbiJ9.fi0afXSQetfhBPAgCm1rEeiIoYBlFzQi-t1W4Q12CV0"
-api_key = os.environ["AIPROXY_TOKEN"] 
+api_key = os.getenv["AIPROXY_TOKEN"] 
 print(api_key)
 response = requests.post("https://aiproxy.sanand.workers.dev/openai/v1/chat/completions",
     headers={"Authorization": f"Bearer {api_key}"},
