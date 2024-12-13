cb4c8abb3276f94d2fa9e410cd34ac55dbb2e920
Nandhini-Ammaiappan <24f1002469@ds.study.iitm.ac.in>
2024-12-14 01:40:55 +0530

    Update autolysis.py

--- autolysis.py
+++ autolysis.py
@@ -76,7 +76,7 @@ def save_markdown(file_path, content):
     subprocess.run(["git", "config", "--global","user.name", REPO_OWNER])
     if not os.path.exists(folder_name[0]):
         os.makedirs(folder_name[0])
-    current_path = "./" + folder_name[0] + "/"
+    current_path = "./" + file_path[0] + "/"
     readme_file = os.path.join(current_path, "README.md")
     with open(readme_file, "w") as f:
         f.write(content)
