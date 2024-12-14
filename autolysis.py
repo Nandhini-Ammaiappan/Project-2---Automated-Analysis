d6b0d9d42114911cdc046ece981eef1fb414917a
Nandhini-Ammaiappan <24f1002469@ds.study.iitm.ac.in>
2024-12-15 00:51:09 +0530

    updated

--- autolysis.py
+++ autolysis.py
@@ -227,13 +227,12 @@ def save_markdown(file_name, content):
     with open(readme_file, "w") as file:
         file.write(content)
     try:
-
         #subprocess executed to push the README from github to git repository and commit the same
         subprocess.run(["git", "config", "--global","user.name", REPO_OWNER],check=True)
         #subprocess.run(["git", "add", repo_folder_path])
         subprocess.run(["git", "add", readme_file],check=True)
         subprocess.run(["git", "commit", "-m", "Add README file"],check=True)
-        subprocess.run(["git","remote","add",file_name_only,remote_url],check=True)
+        #subprocess.run(["git","remote","add",file_name_only,remote_url],check=True)
         subprocess.run(["git", "push", "-u", file_name_only, "main"],check=True)
     except subprocess.CalledProcessError as e:
         print(f"An error occurred: {e}")
